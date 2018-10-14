abb498ce77b83aaa44844cafb7565214385dd9c4
Tianqi Guo <tqguo246@gmail.com>
2018-10-13 21:21:25 -0400

    Update P0001.py

--- P0001.py
+++ P0001.py
@@ -10,9 +10,15 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        """     
-        N_num = len(nums)
+        """    
+        
+        N_num = len(nums)        
+        # brute force search
         for i in range(N_num-1):
             for j in range(i+1,N_num):
+                # return indices when two number sums equal target
                 if nums[i] + nums[j] == target:
                     return([i,j])
+                    
+test = Solution()
+print(test.twoSum([2, 7, 11, 15],9))
