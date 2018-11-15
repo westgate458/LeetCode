16fcb5e344fc81b007283669d807c48088eff300
Tianqi Guo <tqguo246@gmail.com>
2018-11-14 22:46:59 -0500

    comments added

--- P0022.py
+++ P0022.py
@@ -10,21 +10,40 @@ class Solution(object):
         """
         :type n: int
         :rtype: List[str]
-        """        
+        """       
+        # initial answer list with n == 1
+        # index i of combo corresponds to n = i + 1
         combo = [['()']]
+        
+        # for each n get combinations from previous n's
         for i in range(1,n):
+            
+            # combination list for current n
             new_combo = []
+            # combination list from previous n
             lst_combo = combo[i-1]
+            
+            # Part 1 of combinations for current n == i + 1            
             for j in range(len(lst_combo)):
+                # ( + previous combinations + )
                 new_combo.append('('+lst_combo[j]+')')            
+            
+            # Part 2 of combinations for current n == i + 1
             for j in range(i):
+                # sets of (combo from n == j) and (combo from n == i - j - 1)
                 combo_1 = combo[j]
                 combo_2 = combo[i-j-1]
+                # all combinations formed by elements from the two sets
                 for ii in range(len(combo_1)):
                     for jj in range(len(combo_2)):
+                        # add to answer list if it doesn't already exist
                         if not (combo_1[ii]+combo_2[jj] in new_combo):
-                            new_combo.append(combo_1[ii]+combo_2[jj])                        
+                            new_combo.append(combo_1[ii]+combo_2[jj])   
+            
+            # add the combo for current n to the answer list
             combo.append(new_combo)            
+            
+        # return combinations for n = n - 1 + 1
         return combo[n-1]
 
 n = 3
--- P0023.py
+++ P0023.py
@@ -17,66 +17,98 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
+        # number of lists and deal with trivial case
         ls = len(lists)
         if ls == 0:
             return None
         
-        num_inf = float('inf')        
+        # set of active pointers
         ps = lists
-        head = ListNode(-num_inf)
+        
+        # initial answer list with arbitrary value
+        head = ListNode(-float('inf'))
         p = head
         
+        # sorted array formed by one number from each list 
         nums = []      
+        # record of which list each number comes from
         inds = []
+        
+        # take the first number of each list
+        # and record which list it comes from
         for i in range(ls):
             if ps[i]:
                 nums.append(ps[i].val)
                 ps[i] = ps[i].next
                 inds.append(i)
+        
+        # sort the number list from small to large
+        # and the index list accordingly        
         yx = zip(nums, inds)
         yx.sort()
         nums = [y for y, x in yx]
         inds = [x for y, x in yx]
         
+        # add numbers to answer list until no number is left
+        # with insertion of new numbers, the list is maintained sorted along the way
         while nums:
             
+            # take the first value in list
+            # and its list index
             num = nums.pop(0)
             ind = inds.pop(0)
             
+            # add this value to answer list
             p.next = ListNode(num)
             p = p.next            
             
+            # if the original list has not reached the end
+            # take the remaining first number from that list
             if ps[ind] != None:
                 num = ps[ind].val
                 ps[ind] = ps[ind].next
-            else:
+            else:                
                 continue
             
+            # number of remaining numbers in the sorted list
             l_nums = len(nums) 
             
+            # if the number list is empty 
+            # or the to-be-inserted number is smaller than the first number
             if (l_nums == 0) or (num <= nums[0]):
+                # insert the new number as the new first number
                 nums.insert(0,num)
                 inds.insert(0,ind)   
             else:
+                # if the to-be-inserted number is larger than the last number
                 if num >= nums[-1]:
+                    # insert the new number as the new last number
                     nums.insert(l_nums,num)
                     inds.insert(l_nums,ind)  
                 else:
+                    # search where to insert the new number
                     left = 0
                     right = l_nums - 1           
+                    # each time reduce the search range by half
+                    # until the left and right bounds meet
                     while right - left > 1:
+                        # compare the new number with the median of the current range
                         mid = (left + right)/2
+                        # update the search range accordingly
                         if nums[mid] <= num:
                             left = mid
                         else:
                             right = mid
+                    # insert the new number to the sorted list
+                    # at desired positioin
                     if nums[left] < num:
                         nums.insert(left+1,num)
                         inds.insert(left+1,ind)
                     else:
                         nums.insert(left,num)
                         inds.insert(left,ind)        
-                
+        
+        # return the combined sorted list of nodes
         return head.next          
          
  
