c8a94cfc8c66d7f183ef37c90aec00305de43fc2
Tianqi Guo <tqguo246@gmail.com>
2018-12-08 21:30:29 -0500

    initial submission

--- P0040.py
+++ P0040.py
@@ -1,46 +1,47 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec  7 16:02:49 2018
+Created on Sat Dec  8 21:05:54 2018
 
 @author: Tianqi Guo
 """
 
 class Solution(object):
-    def combinationSum(self, candidates, target):
+    def combinationSum2(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        
         def DFS(candidates, remaining, combo):
             
-            if remaining == 0:
+            if remaining == 0:                
                 if not combo in self.ans:
                     self.ans.append(list(combo))
                 return None
             
             if remaining > 0:
-                for num in candidates:
-                    if num > remaining:
+                l = len(candidates)
+                for i in range(l):
+                    if candidates[i] > remaining:
                         break
-                    if (not combo) or (num >= combo[-1]): 
-                        combo.append(num)
+                    if (not combo) or (candidates[i] >= combo[-1]): 
+                        combo.append(candidates[i])                        
+                        num = candidates.pop(i)
                         DFS(candidates, remaining - num, combo)
-                        combo.pop(-1)      
+                        combo.pop(-1)     
+                        candidates.insert(i,num)
                         
         self.ans = []
         candidates.sort()
         DFS(candidates, target, list([]))
         return self.ans
 
-candidates = [4,2,8]
-target = 8
+candidates = [2,5,2,1,2]
+target = 5
 
 test = Solution()
-print(test.combinationSum(candidates, target))
+print(test.combinationSum2(candidates, target))
         
         
                     
                 
-        
