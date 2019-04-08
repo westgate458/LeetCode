45f57d575e252846799c05defebd8faa8c19d909
Tianqi Guo <tqguo246@gmail.com>
2019-04-08 00:03:51 -0400

    Create P0117.py

---
+++ P0117.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 22:10:10 2019
+
+@author: Tianqi Guo
+"""
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """     
+        
+        
+        # Solution 1
+        # deal with trivial case
+        if not root:            
+            return 
+        # stack storing nodes at each level
+        s = [root]  
+        # continue traversing while still nodes left
+        while s: 
+            new_s = []            
+            for n in s:
+                if n.left:
+                    new_s.append(n.left)
+                if n.right:
+                    new_s.append(n.right)                    
+            for i in range(len(new_s)-1):
+                new_s[i].next  = new_s[i+1]            
+            s = new_s 
+        return root
+    
+        # Solution 2
+        # deal with trivial case
+        if not root:            
+            return         
+        current_level = root
+        if root.left:
+            next_level = root.left
+        elif root.right:
+            next_level = root.right    
+        else:
+            next_level = None 
+        while current_level.left or current_level.right or current_level.next or next_level:
+            current_level_next = current_level.next            
+            if current_level_next:                
+                while (not (current_level_next.left or current_level_next.right)) and current_level_next.next:
+                    current_level_next = current_level_next.next             
+            if current_level.left:               
+                if not next_level:
+                    next_level = current_level.left
+                if current_level.right:
+                    current_level.left.next = current_level.right                    
+                elif current_level_next:
+                    if current_level_next.left:
+                        current_level.left.next = current_level_next.left
+                    elif current_level_next.right:
+                        current_level.left.next = current_level_next.right
+            if current_level.right:                
+                if not next_level:
+                    next_level = current_level.right
+                if current_level_next:                 
+                    if current_level_next.left:
+                        current_level.right.next = current_level_next.left
+                    elif current_level_next.right:
+                        current_level.right.next = current_level_next.right
+            if current_level_next:                
+                current_level = current_level.next
+            else:                
+                current_level = next_level
+                next_level = None
+        return root  
