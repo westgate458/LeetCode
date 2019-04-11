73ccea53a7acc60adb3d5f5ddc5e0d44bc4840da
Tianqi Guo <tqguo246@gmail.com>
2019-04-10 20:02:57 -0400

    Create P0120.py

---
+++ P0120.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 19:36:52 2019
+
+@author: Tianqi Guo
+"""
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """  
+             
+        for i in range(len(triangle)-2,-1,-1):            
+            for j in range(i+1):                
+                triangle[i][j] += min(triangle[i+1][j],triangle[i+1][j+1])       
+                
+        return triangle[0][0]
+       
+
+triangle = [
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]        
+test = Solution()
+print test.minimumTotal(triangle)
