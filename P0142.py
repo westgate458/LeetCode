cc5c23ec0df2a6e1f1750f42ab247c31dd52cd31
Tianqi Guo <tqguo246@gmail.com>
2019-05-07 16:35:20 -0400

    Update P0142.py

--- P0142.py
+++ P0142.py
@@ -5,3 +5,25 @@ Created on Tue May  7 16:05:08 2019
 @author: Tianqi Guo
 """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def __init__(self):
+        self.visited = set()
+        
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        if head in self.visited:            
+            return head
+        else:
+            self.visited |= set([head])            
+            return self.detectCycle(head.next)
