554af5747076dc9010f2765ecbd1b8f97eeaf1c9
Tianqi Guo <tqguo246@gmail.com>
2019-05-07 21:38:53 -0400

    Update P0142.py

--- P0142.py
+++ P0142.py
@@ -11,19 +11,40 @@ Created on Tue May  7 16:05:08 2019
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def __init__(self):
-        self.visited = set()
-        
+
+# Solution 1
+class Solution(object):    
     def detectCycle(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
-        """
-        if not head:
-            return None
-        if head in self.visited:            
-            return head
-        else:
-            self.visited |= set([head])            
-            return self.detectCycle(head.next)
+        """        
+        p1 = p2 = head        
+        while p2 and p2.next:            
+            p1 = p1.next
+            p2 = p2.next.next            
+            if p1 == p2:                
+                p1 = head
+                while p1 != p2:
+                    p1 = p1.next
+                    p2 = p2.next
+                return p1        
+        return None
+
+# Solution 2
+#class Solution(object):
+#    def __init__(self):
+#        self.visited = set()
+#        
+#    def detectCycle(self, head):
+#        """
+#        :type head: ListNode
+#        :rtype: ListNode
+#        """
+#        if not head:
+#            return None
+#        if head in self.visited:            
+#            return head
+#        else:
+#            self.visited |= set([head])            
+#            return self.detectCycle(head.next)
