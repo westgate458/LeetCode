f1bb5a7b9a4402fb9f5de58228f4a0fa18e05ccc
Tianqi Guo <tqguo246@gmail.com>
2019-05-31 17:56:00 -0400

    Create P0162.py

---
+++ P0162.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 31 16:50:17 2019
+
+@author: Tianqi Guo
+"""
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        nums = [-float('inf')] + nums + [-float('inf')]  
+        
+        for i in xrange(1,len(nums)-1):
+            if nums[i-1] < nums[i] > nums[i+1]:
+                return i-1
+            
+
+nums = [1,3,2,1]
+nums = [3,4,3,2,1]
+nums = [2,1]
+nums = [3,2,1]
+test = Solution()
+print test.findPeakElement(nums)
