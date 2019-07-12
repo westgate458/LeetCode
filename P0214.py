293175cddb52b58689649af320a38d7598fdad65
Tianqi Guo <tqguo246@gmail.com>
2019-07-12 17:39:33 -0400

    Update P0214.py

--- P0214.py
+++ P0214.py
@@ -10,53 +10,42 @@ class Solution(object):
         """
         :type s: str
         :rtype: str
-        """
+        """        
         
-        for i in range(len(s)):  
-            ss = s[len(s)-i:][::-1] + s            
-            if ss == ss[::-1]:
-                return ss
-        return ''
+        # Solution 1: beats 99%
+        i = 0
+        for c in s[::-1]:
+            if c == s[i]:
+                i += 1       
+        if i == len(s):
+            return s
+        else:        
+            return s[i:][::-1] + self.shortestPalindrome(s[:i]) + s[i:]
         
-#        if s == s[::-1]:
-#            return s
-#        
-#        ans = float('inf')
-#        ss = ''
-#        
-#        for i in range(int(round(len(s)/2.0))):   
-#            
-#            l = 1
-#            while i-l>=0 and s[i-l] == s[i+l]:
-#                l += 1                  
-#                
-#            if l == i+1 and len(s)-(2*l+1) < ans:
-#                ans = len(s)-(2*l+1)
-#                ss = s[i+1:][::-1] + s[i:]
-#            
-#            l = 1
-#            while i-l+1>=0 and i+l < len(s) and s[i-l+1] == s[i+l]:
-#                
-#                l += 1                  
-#            if l == i+2 and len(s)-2*l < ans:                
-#                ans = len(s)-2*l
-#                ss = s[i+1:][::-1] + s[i+1:]
-#            
-#        return ss
+        # Solution 2: KMP beats 78%
+        ss = s + '#' + s[::-1]        
+        ls = [0] * len(ss)        
+        ll, p = 0, 1  
+        while p < len(ss):            
+            if ss[ll] == ss[p]:
+                ll += 1
+                ls[p] = ll                
+                p += 1                
+            elif ll > 0:                
+                ll = ls[ll-1]   
+            else:                
+                p += 1
+        return s[ls[-1]:][::-1] + s
 
-
-
-
-
-
-
-s = "ab"
+s = "aacd" 
+s = ""
 s = "aba"
+s = "ab"
 s = "abb"
 s = "abcd" 
 s = "aacecaaa" 
-s = "aacd" 
-s = ""
+s = "babbbabbaba"
+
 test = Solution()
 print test.shortestPalindrome(s)
 
