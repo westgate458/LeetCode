dee4c28413857df291605f49caffad2a6a31218c
Tianqi Guo <tqguo246@gmail.com>
2019-07-26 21:43:26 -0400

    Update P0224.py

--- P0224.py
+++ P0224.py
@@ -11,41 +11,74 @@ class Solution(object):
         :rtype: int
         """
         
-        ss = [0]        
-        current = ''  
+        # Solution 1 beats 99%: only stack for sign and res
+        res, sign, temp, ord0 = 0, 1, 0, ord('0')
+        sgn_stack = []
+        rst_stack = []
         
-        for c in '('+s+')':
-           
-            if c in [' ', '+', '-','(',')']:
-                if current:    
-                    num = int(current)
-                    if ss[-1] == '-':
-                        ss.pop(-1)
-                        num = -num
-                    if ss[-1] != '(':
-                        ss[-1] += num
-                    else:
-                        ss.append(num)
-                    current = ''
-                if c == ')':
-                    num = ss.pop(-1)                                          
-                    ss.pop(-1)                   
-                    if ss[-1] == '-':
-                        ss.pop(-1)
-                        num = -num
-                    if ss[-1] != '(':
-                        ss[-1] += num
-                    else:
-                        ss.append(num)
-                if c in ['-', '(']:                     
-                    ss.append(c)
+        for c in s:   
+            
+            if c.isdigit():
+                temp = temp*10 + ord(c) - ord0
             else:
-                current += c        
+                res += sign * temp
+                temp = 0
+                
+                if c == '(':                
+                    rst_stack.append(res)
+                    sgn_stack.append(sign)
+                    res = 0 
+                elif c == ')':
+                    res = rst_stack.pop(-1) + sgn_stack.pop(-1) * res 
+                    
+                if c == '-':
+                    sign = -1
+                else:
+                    sign = 1
+                
+        return res + sign * temp
+                
         
-        return ss[-1]
+        
+        # Solution 2 beats 38.6%: stack for all
+#        ss = [0]        
+#        current = ''  
+#        
+#        for c in '('+s+')':
+#           
+#            if c in [' ', '+', '-','(',')']:
+#                if current:    
+#                    num = int(current)
+#                    if ss[-1] == '-':
+#                        ss.pop(-1)
+#                        num = -num
+#                    if ss[-1] != '(':
+#                        ss[-1] += num
+#                    else:
+#                        ss.append(num)
+#                    current = ''
+#                if c == ')':
+#                    num = ss.pop(-1)                                          
+#                    ss.pop(-1)                   
+#                    if ss[-1] == '-':
+#                        ss.pop(-1)
+#                        num = -num
+#                    if ss[-1] != '(':
+#                        ss[-1] += num
+#                    else:
+#                        ss.append(num)
+#                if c in ['-', '(']:                     
+#                    ss.append(c)
+#            else:
+#                current += c        
+#        
+#        return ss[-1]
+
+
+s = "1-(1+1)"
 
 s= " 2-1 + 2 "
-s = "1-(1+1)"
+
 s = "(1+(4+5+2)-3)+(6+8)"
 
 test = Solution()
