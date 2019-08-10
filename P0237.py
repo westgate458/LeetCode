496f23eb397d5581f0cedd7636fb71f169738837
Tianqi Guo <tqguo246@gmail.com>
2019-08-10 16:59:26 -0400

    Create P0237.py

---
+++ P0237.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 10 16:55:33 2019
+
+@author: Tianqi Guo
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        
+        node.val, node.next = node.next.val, node.next.next
