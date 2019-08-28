329b211c0f3a72fccb38868a377c9afd80f065c5
Tianqi Guo <tqguo246@gmail.com>
2019-08-27 20:54:43 -0400

    Create P0268.py

---
+++ P0268.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 20:34:48 2019
+
+@author: Tianqi Guo
+"""
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """   
+        
+        # Solution 1 beats 98.03%: length and sum
+        n = len(nums)       
+        return n * (n + 1)/2 - sum(nums)
+    
+        # Solution 2 beats 80.61%: min, max, sum
+        a, b, s = max(nums), min(nums), sum(nums)
+        d = (0 + a) * (a + 1)/2 - s        
+        if b != 0:
+            return 0
+        elif d == 0:
+            return a + 1
+        else:
+            return d
