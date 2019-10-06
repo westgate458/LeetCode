30221086fadfcd78aa04a23cdeb260e6e3d632e0
Tianqi Guo <tqguo246@gmail.com>
2019-10-06 16:37:45 -0400

    Create P0319.py

---
+++ P0319.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  6 16:36:35 2019
+
+@author: Tianqi Guo
+"""
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """                 
+        return int(n**0.5)
