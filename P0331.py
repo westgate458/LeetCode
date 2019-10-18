13ffa9cf4583da32b37d37a6644c61e5f20ff453
Tianqi Guo <tqguo246@gmail.com>
2019-10-17 21:06:31 -0400

    Create P0331.py

---
+++ P0331.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 20:55:04 2019
+
+@author: Tianqi Guo
+"""
+
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """        
+        # Solution 1 beats 99.43%: counting slots
+        s = 1
+        for is_null in map(lambda c: c == '#', preorder.split(',')):
+            if not s: return False     
+            s = s - 1 if is_null else s + 1              
+        return not s
+        
+#        # Solution 2 beats 94.59%: using stack
+#        nodes = preorder.split(',')
+#        l = len(nodes)
+#        if nodes[0] == '#':
+#            return l == 1  
+#        s = [[nodes[0],0]]
+#        p = 0        
+#        for p in xrange(1,l):
+#            if nodes[p] == '#':
+#                if not s:
+#                    return False
+#                s[-1][1] += 1
+#                while s and s[-1][1] == 2:
+#                    s.pop()                    
+#                    if s:
+#                        s[-1][1] += 1
+#                    elif p < l - 1:
+#                        return False
+#            else:
+#                s.append([nodes[0],0])
+#        return not s
