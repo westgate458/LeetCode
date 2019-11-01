ee8917e7e6d64ad65c176b3f002873afb65a477e
Tianqi Guo <tqguo246@gmail.com>
2019-11-01 15:51:37 -0400

    Create P0349.py

---
+++ P0349.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 15:51:14 2019
+
+@author: Tianqi Guo
+"""
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return set(nums1) & set(nums2)
