fabd036c9f06720881f0a7bbe013abaa2ac6420b
Tianqi Guo <tqguo246@gmail.com>
2019-11-12 21:14:29 -0500

    Create P0368.py

---
+++ P0368.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 21:10:43 2019
+
+@author: westg
+"""
+
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        # Solution 1 beats 98.98%: dp + dictionary
+        dp = {-1:set()}        
+        for x in sorted(nums):
+            dp[x] = max([dp[y] for y in dp if x%y == 0], key=len) | set([x])        
+        return max(dp.values(), key=len)
+            
+#        # Solution 2 beats 88.52%: naive dp
+#        if not nums:
+#            return []
+#        
+#        l = len(nums)
+#        dp, ps = [1] * l, [-1] * l         
+#        nums.sort()        
+#        for i in xrange(l-1):
+#            for j in xrange(i+1,l):
+#                if (nums[j] % nums[i] == 0) and (dp[i]+1 > dp[j]):                          
+#                    dp[j], ps[j] = dp[i]+1, i                         
+#
+#        i = dp.index(max(dp)) 
+#        ans = []
+#        while i != -1:
+#            ans.append(nums[i])
+#            i = ps[i]
+#                    
+#        return ans[::-1]
