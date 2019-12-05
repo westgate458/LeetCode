aa795e1182fee4bb2a4b0815bebc090ec4d530bf
Tianqi Guo <tqguo246@gmail.com>
2019-12-04 20:58:31 -0500

    Create P0383.py

---
+++ P0383.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 20:54:16 2019
+
+@author: Tianqi Guo
+"""
+
+
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """      
+        # Solution 1 beats 100%: built-in function        
+        for c in set(ransomNote):
+            if ransomNote.count(c) > magazine.count(c):
+                return False        
+        return True
+            
+        # Solution 2 beats 85.26%: dictionary
+        from collections import defaultdict          
+        
+        if not set(ransomNote).issubset(set(magazine)):
+            return False
+        n = defaultdict(int)   
+        for c in magazine:
+            n[c] += 1
+        for c in ransomNote:
+            n[c] -= 1
+            if n[c] < 0:
+                return False   
+        return True
+            
+        
