efad35d6bf8c796b4b4dfe8bfc187d25191b1aaa
Tianqi Guo <tqguo246@gmail.com>
2019-12-10 19:50:17 -0500

    Create P0389.py

---
+++ P0389.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 19:48:18 2019
+
+@author: Tianqi Guo
+"""
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        # Solution 1 beats 94.64%: 1-liner
+        return chr(sum(map(ord,t))-sum(map(ord,s)))
+        
+        # Solution 2 beats 94.64%: string function
+        for a in set(t):
+            if s.count(a) < t.count(a):
+                return a
