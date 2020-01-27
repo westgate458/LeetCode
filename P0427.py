535c5e416c2ff925a47bbb7d8937dda2c7df2217
Tianqi Guo <tqguo246@gmail.com>
2020-01-26 20:33:49 -0500

    Create P0427.py

---
+++ P0427.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 26 20:32:31 2020
+
+@author: Tianqi Guo
+"""
+
+"""
+# Definition for a QuadTree node.
+class Node(object):
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+class Solution(object):
+    def construct(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: Node
+        """    
+        for row in grid:
+            for n in row:
+                if n != grid[0][0]:
+                    ll = len(grid)/2  
+                    return Node(None, False, 
+                                self.construct([row[:ll] for row in grid[:ll]]), 
+                                self.construct([row[ll:] for row in grid[:ll]]), 
+                                self.construct([row[:ll] for row in grid[ll:]]), 
+                                self.construct([row[ll:] for row in grid[ll:]]))
+                    
+        return Node(grid[0][0]==1 , True, None, None, None, None)
